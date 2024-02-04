084b0d3f67701e2a5b57e415390d2436dff6af49
vakhrushevyp <81510633+vakhrushevyp@users.noreply.github.com>
2024-02-04 12:43:10 +0400

    Аннотации, Assertions.
    Без AsserThat, без параметризации, без выбора браузера.

--- src/test/java/org/ibs/appline/RgsTest.java
+++ src/test/java/org/ibs/appline/RgsTest.java
@@ -6,13 +6,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.function.Function;
+
 
 public class RgsTest extends BaseTests {
 
@@ -21,7 +19,7 @@ public class RgsTest extends BaseTests {
     void rgsTest() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(15));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(2));
@@ -48,16 +46,32 @@ public class RgsTest extends BaseTests {
         userEmail.sendKeys("qweqweqwe");
         WebElement city = driver.findElement(By.xpath("//input[@class='vue-dadata__input']"));
         city.sendKeys("Ижевск");
-        WebElement cityDropdown = driver.findElement(By.xpath("//span[contains(., 'г Ижевск')] "));
+        sleep(1000);
+        WebElement dropDownAll = driver.findElement(By.xpath("//div[@class='vue-dadata__suggestions']"));
+        Actions action= new Actions(driver);
+        action.moveToElement(dropDownAll).build().perform();
+        sleep(1000);
+        WebElement cityDropdown = driver.findElement(By.xpath("//span[@class='vue-dadata__suggestions-item']"));
         cityDropdown.click();
+        sleep(1000);
         WebElement checkBox = driver.findElement(By.xpath("//label[@class='checkbox']"));
-        checkBox.click();
+        action.moveToElement(checkBox,-20,0).click().build().perform();
+        sleep(3000);
 
-        Assertions.assertEquals("Иванов Иван Иванович", userName.getText(), "ФИО введено неверно");
-        Assertions.assertEquals("9509502956", userTel.getText(),"Номер телефона введен неверно");
-        Assertions.assertEquals("qweqweqwe", userEmail.getText(),"Почта введена неверно");
-        Assertions.assertEquals("Ижевск", city.getText(),"Город введен неверно" );
-        Assertions.assertTrue(checkBox.isSelected(),"Чекбокс согласий не отмечен");
+        Assertions.assertEquals("Иванов Иван Иванович", userName.getDomProperty("value"), "ФИО введено неверно");
+        Assertions.assertEquals("+7 (950) 950-2956", userTel.getDomProperty("value"),"Номер телефона введен неверно");
+        Assertions.assertEquals("qweqweqwe", userEmail.getDomProperty("value"),"Почта введена неверно");
+        Assertions.assertEquals("г Ижевск", city.getDomProperty("value"),"Город введен неверно" );
+        WebElement checkboxChecked = driver.findElement(By.xpath("//input[@class='input']"));
+        Assertions.assertTrue(checkboxChecked.isSelected(),"Чекбокс согласий не отмечен");
+
+
+
+        WebElement button = driver.findElement(By.xpath("//button[@type='submit']"));
+        Assertions.assertFalse(button.isEnabled(), "Кнопка доступна для нажатия");
+
+        WebElement textError = driver.findElement(By.xpath("//span[@class='input__error text--small']"));
+        Assertions.assertEquals("Введите корректный адрес электронной почты",textError.getText(),"Сообщение об ошибке ввода почты неверное");
 
 
         driver.quit();
